df65fcc2831b45e81841bda2b0f1f5b9ff2cc42e
sanlouise <sandra.adamshallie@gmail.com>
2017-07-14 07:26:58 -0400

    Improve logic, add IDs to button elements

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
+let inputArr = [];
+
 // FUNCTION DEFINITIONS
 const clear = () => {
   inputArr = [];
   document.getElementById('input').innerHTML = inputArr;
 }
 
-const calculate = (element) => {
-  console.log('element', element.innerHTML);
-  inputArr.push(this.innerHTML);
+const calculate = (button) => {
+  console.log('button', button.innerHTML);
+  inputArr.push(button.innerHTML);
+  inputField.innerHTML = inputArr.map(Number).join('');
 }
 
 const evaluate = () => {
   let result = eval(inputArr);
-  inputArr.innerHTML = result
+  inputField.innerHTML = result;
 }
 
 // VARIABLE DECLARATION
@@ -21,7 +24,31 @@ digitButtonOne.addEventListener('click', () => calculate(digitButtonOne));
 const digitButtonTwo = document.getElementById('two');
 digitButtonTwo.addEventListener('click', () => calculate(digitButtonTwo));
 
+const digitButtonThree = document.getElementById('three');
+digitButtonThree.addEventListener('click', () => calculate(digitButtonThree));
+
+const digitButtonFour = document.getElementById('four');
+digitButtonFour.addEventListener('click', () => calculate(digitButtonFour));
+
+const digitButtonFive = document.getElementById('five');
+digitButtonFive.addEventListener('click', () => calculate(digitButtonFive));
+
+const digitButtonSix = document.getElementById('six');
+digitButtonSix.addEventListener('click', () => calculate(digitButtonSix));
+
+const digitButtonSeven = document.getElementById('seven');
+digitButtonSeven.addEventListener('click', () => calculate(digitButtonSeven));
+
+const digitButtonEight = document.getElementById('eight');
+digitButtonEight.addEventListener('click', () => calculate(digitButtonEight));
+
+const digitButtonNine = document.getElementById('nine');
+digitButtonNine.addEventListener('click', () => calculate(digitButtonNine));
+
 const clearButton = document.getElementById('clear');
 clearButton.addEventListener('click', clear);
 
-let inputArr = [];
+const inputField = document.getElementById('input');
+inputField.innerHTML = inputArr;
+
+console.log(inputArr);
