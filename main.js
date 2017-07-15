171072e6bbdd9463a6510638fae67d0d7d72a48b
sanlouise <sandra.adamshallie@gmail.com>
2017-07-15 18:48:36 -0400

    Refactor main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let inputArr = [], dataStore = [];
+let inputArr = [], dataStore = [], currentDataStorageItem, historyPosition = 1;
 
 const operationButtons = document.querySelectorAll(".btn")
 operationButtons.forEach(function(button){
@@ -16,7 +16,6 @@ const calculate = (button) => {
 }
 
 // STORE
-let currentDataStorageItem, historyPosition = 1
 const dataStoreButton = document.getElementById('dataStore');
 dataStoreButton.addEventListener('click', (event) => getDataStore())
 
@@ -34,43 +33,41 @@ const getOperatorIndex = (operator) => {
   return operator === '+' || operator === '-' || operator === '*' || operator === '/' || operator === '%'
 }
 
+const displayResult = () => {
+  inputField.innerHTML = result;
+  dataStore.push(result);
+  inputArr = [];
+}
+
+const getResultForSqrt = () => {
+  inputArr = inputArr.slice(1);
+  result = Math.sqrt(inputArr.join(''));
+  displayResult();
+}
+
+const getResult= () => {
+  let operatorIndex = inputArr.findIndex(getOperatorIndex);
+  let operator = inputArr[operatorIndex];
+  let a = inputArr.slice(0, operatorIndex).join('');
+  let b = inputArr.splice(operatorIndex + 1).join('');
+
+  switch (operator) {
+    case "+": result = a + b; break;
+    case "-": result = a - b; break;
+    case "*": result = a * b; break;
+    case "/": result = a / b; break;
+    case "%": result = a % b; break;
+  }
+  displayResult();
+}
+
 const evaluate = () => {
   historyPosition = 1
   let result;
   if (inputArr[0] != "√") {
-
-    let operatorIndex = inputArr.findIndex(getOperatorIndex);
-    let operator = inputArr[operatorIndex];
-    let a = inputArr.slice(0, operatorIndex).join('');
-    let b = inputArr.splice(operatorIndex + 1).join('');
-
-    switch (operator) {
-      case "+":
-        result = a + b;
-        break;
-      case "-":
-        result = a - b;
-        break;
-      case "*":
-        result = a * b;
-        break;
-      case "/":
-        result = a / b;
-        break;
-      case "%":
-        result = a % b;
-        break;
-    }
-
-    inputField.innerHTML = result;
-    dataStore.push(result);
-    inputArr = [];
+    getResult();
   } else {
-    inputArr = inputArr.slice(1);
-    let result = Math.sqrt(eval(inputArr.join('')));
-    inputField.innerHTML = result;
-    dataStore.push(result);
-    inputArr = [];
+    getResultForSqrt();
   }
 }
 
